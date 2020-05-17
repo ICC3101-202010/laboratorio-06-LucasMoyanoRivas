db8ba7c70e6d5c6b57139c6ae7f4a591ac0232d3
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-05-16 20:52:18 -0400

    primer commit

---
+++ SextoLaboratorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SextoLaboratorio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
